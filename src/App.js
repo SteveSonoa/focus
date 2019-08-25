f82b05d42d3dc7b0adcfd4fe4bdbe2cc5b67209f
Steve Marshall <smarshall@axway.com>
2019-08-24 20:46:44 -0700

    Updated to work as a standalone project.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GamesFocus from './focus/view/GamesFocus';
+
+function App() {
+	return (
+		<div className="App">
+			<GamesFocus />
+		</div>
+	);
+}
+
+export default App;
